572a36e1acb471e64cf5ce4756d3fb4873d92b69
Noperman <56102176+noperman@users.noreply.github.com>
2021-03-31 10:29:06 +0700

    Update init.php

--- init.php
+++ init.php
@@ -1,14 +1,6 @@
 <?php
-/**
- * Author: P@trick
- * Date: 2020-03-21
- * Time: 11:52 AM
- */
-
-/** Set drive you need to auto delete */
 $disk = disk_total_space("C:");
 $free = disk_free_space("C:");
-/** 1 Gb = 1073741824 Kb */
 $gb = 1073741824;
 
 $size = (integer)round($disk/$gb);
@@ -17,39 +9,43 @@ $freesize = (integer)round($free/$gb);
 $sparator = "\n=============================\n";
 echo "Free space :".$freesize." Gb of".$size." Gb".$sparator;
 
-/** Set limit free size variable and conditiion */
 $NeededSize = 150;
 if($freesize < $NeededSize){
-    /** Set directory you need to auto delete */
-    $dir = "/dirname/";
+    $dir = "/backup/daily backup/";
     if (is_dir($dir)){
         if ($dh = opendir($dir)){
             while (($file = readdir($dh)) !== false){
-                /** Set except file */
+                // echo $file.$sparator;
                 if ($file != "." && $file != ".." && $file != ".tmp.drivedownload" && $file != "desktop.ini") {
-                    $files[filemtime($dir.$file)] = $file;
+                    // echo $file.$sparator;
+                    // $files[filemtime($dir.$file)] = $file;
+                    $files[] = array('file' => $file, 'time' => filemtime($dir.$file));
                 }
             }
             closedir($dh);
-
-            ksort($files);            
+            
+            uasort($files, function($file1, $file2) {
+                if ( $file1['time'] == $file2['time'] )
+                return 0;
+                return $file1['time'] < $file2['time'] ? -1 : 1;
+            });
+                       
             $DeletedSize = 0;
+            $no = 1;
             foreach($files as $file) {
-                $lastModified = date('F d Y, H:i:s',filemtime($dir.$file));
-                /** 
-                 * Set extention file that you will able to delete 
-                 * For Ex : .bak
-                */
-                if(strlen($file)-strpos($file,".bak")== 4){
-                    $DeletedSize = $DeletedSize + (integer)filesize($dir.$file);
+                $lastModified = date('F d Y, H:i:s',filemtime($dir.$file['file']));
+                if(strlen($file['file'])-strpos($file['file'],".bak")== 4){
+                    $DeletedSize = $DeletedSize + (integer)filesize($dir.$file['file']);
 
-                    unlink($dir.$file);
+                    unlink($dir.$file['file']);
+                    // echo $no.' '.$dir.$file['file']."\n";
 
-                    echo $file." Created at ".$lastModified." =====> Deleted! \n";
-                    if((integer)round($DeletedSize/$gb) > $NeededSize){
+                    echo $no." ".$file['file']." Created at ".$lastModified." =====> Deleted! \n";
+                    if(($freesize + (integer)round($DeletedSize/$gb)) >= $NeededSize){
                         break;
                     }
                 }
+                $no++;
             }
             echo "Deleted Size : ".(integer)round($DeletedSize/$gb)." Gb of".$NeededSize." Gb needed!";
         }
